81b729ab4c4b69200cbad9796f707df1ccfee6a1
Nicolas Souza Siqueira <109821803+Nicolassiqueira05@users.noreply.github.com>
2025-08-19 18:03:29 -0300

    window partially finished

--- src/main/java/org/jrec/Main.java
+++ src/main/java/org/jrec/Main.java
@@ -4,19 +4,8 @@ import org.jrec.recorder.Recorder;
 import org.jrec.window.Window;
 
 public class Main {
-    public static void main(String[] args) {
-        Window window = new Window();
-        try {
-            Recorder recorder = new Recorder("output.mp4", 30.0);
-
-            System.out.println("Iniciando gravação...");
-            recorder.start();
-
-            recorder.stop();
-            System.out.println("Gravação concluída: output.mp4");
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws Exception {
+        Recorder recorder = new Recorder("output.mp4", 30.0);
+        Window window = new Window(recorder::start, recorder::stop);
     }
 }
--- src/main/java/org/jrec/window/Window.java
+++ src/main/java/org/jrec/window/Window.java
@@ -1,15 +1,38 @@
 package org.jrec.window;
+
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Window extends JFrame {
-    public Window(){
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setTitle("Java Recorder");
-        this.setSize(500, 100);
-        this.setVisible(true);
-        this.setResizable(true);
+
+    private JButton b1, b2, b3, b4, b5;
+
+    public Window(Runnable m1, Runnable m2) {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setTitle("Java Recorder");
+        setResizable(true);
+
+        JPanel jpanel = new JPanel(new GridLayout(1, 5, 5, 5));
+
+        b1 = new JButton("Iniciar");
+        b1.addActionListener(e -> m1.run());
+        b2 = new JButton("Parar");
+        b2.addActionListener(e -> m2.run());
+        b3 = new JButton("Pausar");
+        b4 = new JButton("Salvar");
+        b5 = new JButton("Sair");
+
+        jpanel.add(b1);
+        jpanel.add(b2);
+        jpanel.add(b3);
+        jpanel.add(b4);
+        jpanel.add(b5);
+
+        add(jpanel);
+
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
     }
+
 }
