5a7d5c92360476c3def02db88c5d88ec33095dd4
Nicolas Souza Siqueira <109821803+Nicolassiqueira05@users.noreply.github.com>
2025-08-20 18:58:40 -0300

    start of window style

--- src/main/java/org/jrec/window/Window.java
+++ src/main/java/org/jrec/window/Window.java
@@ -6,16 +6,25 @@ import java.awt.*;
 public class Window extends JFrame {
 
     private JButton b1, b2, b3, b4, b5;
+    static ImageIcon b1i, b2i, b3i;
     private boolean rec = false;
 
     public Window(Runnable m1, Runnable m2, Runnable m3) {
+
+        b1i = new ImageIcon(this.getClass().getResource("/b1.png"));
+        Image resizedb1i = b1i.getImage().getScaledInstance(40, 40, Image.SCALE_SMOOTH);
+
+        b1i = new ImageIcon(resizedb1i);
+
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Java Recorder");
         setResizable(true);
 
         JPanel jpanel = new JPanel(new GridLayout(1, 5, 5, 5));
 
-        b1 = new JButton("Iniciar");
+        b1 = new JButton();
+        b1.setIcon(b1i);
+        setButtonStyle(b1);
         b1.addActionListener(e -> {
             if(!rec)
             {
@@ -45,6 +54,11 @@ public class Window extends JFrame {
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
+    }
+
+    public void setButtonStyle(JButton button){
+        button.setSize(new Dimension(40, 40));
+        button.setPreferredSize(new Dimension(40, 40));
     }
 
 }
